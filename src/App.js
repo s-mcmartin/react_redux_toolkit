59cf69965b1d5b756a0bef574b1418fffd624937
s-mcmartin <shannoncmcguire@outlook.com>
2023-11-19 10:01:51 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/Layout";
+import PostsList from "./features/post/PostsList";
+import AddPostForm from "./features/post/AddPostForm.js";
+import SinglePostPage from "./features/post/SinglePostPage";
+import EditPostForm from "./features/post/EditPostForm";
+import UsersList from "./features/users/UsersList";
+import UserPage from "./features/users/UserPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          <Route path="edit/:postId" element={<EditPostForm />} />
+        </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+
+        {/*Catch all - replace with 404 component*/}
+        <Route path="*" element={<Navigate to={"/"} replace />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/features/post/PostsExcerpt.js
@@ -0,0 +1,28 @@
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectPostById } from "./postsSlice";
+
+const PostsExcerpt = ({ postId }) => {
+  const post = useSelector((state) => selectPostById(state, postId));
+  return (
+    <article>
+      <h2>{post.title}</h2>
+      <p className="excerpt">{post.body.substring(0, 75)}...</p>
+      <p className="postCredit">
+        <Link to={`post/${post.id}`}>View Post</Link>
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <ReactionButtons post={post} />
+    </article>
+  );
+};
+
+// //allows to not rerender if prop has not changed
+// PostsExcerpt = React.memo(PostsExcerpt);
+export default PostsExcerpt;
+
+//Normalization --> 1. Items stored in lookup table by item id  2. no duplicates
